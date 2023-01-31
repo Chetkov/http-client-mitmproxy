1fb69d98971a9555e628c4b1684a31316420bbe2
Valeriy Chetkov <chetkov.valeriy@gmail.com>
2023-01-31 17:57:07 +0300

    bootstrap fixes

--- bootstrap.php
+++ bootstrap.php
@@ -3,10 +3,14 @@
 declare(strict_types=1);
 
 // Авто-загрузчик из vendor http-client-mitmproxy
-$autoloaderPath = dirname(__DIR__) . '/vendor/autoload.php';
+$autoloaderPath = __DIR__ . '/vendor/autoload.php';
 if (!file_exists($autoloaderPath)) {
     // Авто-загрузчик из vendor проекта, к которому mitmproxy подключен
-    $autoloaderPath = dirname(__DIR__, 4) . '/vendor/autoload.php';
+    $autoloaderPath = dirname(__DIR__, 3) . '/vendor/autoload.php';
+}
+
+if (!file_exists($autoloaderPath)) {
+    throw new RuntimeException('Can not find autoload.php');
 }
 
 require_once $autoloaderPath;
